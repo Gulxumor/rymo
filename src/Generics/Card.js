10b74e51ff673f404c14498884cfba299f9c0d06
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-04-17 10:08:41 +0500

    not finished

--- src/Generics/Card.js
+++ src/Generics/Card.js
@@ -3,6 +3,7 @@ import { Rate } from "antd";
 import { Button } from "./Button";
 import { useTranslation } from "react-i18next";
 import {
+  BtnWrapper,
   CardContainer,
   CardImg,
   CardWrapper,
@@ -15,13 +16,15 @@ const Card = ({ image, title, price, id }) => {
   const { t } = useTranslation();
   // const  navigate = useNavigation()
   return (
-    <CardContainer >
+    <CardContainer>
       <CardImg small src={image} alt="arrival" />
       <CardWrapper>
         <Rate allowHalf defaultValue={3} />
         <Text>{title}</Text>
         <Text>${price}.00</Text>
+        <BtnWrapper>
         <Button onClick={() => navigate(`/shop/${id}`)} >{t("buy_now")}</Button>
+        </BtnWrapper>
       </CardWrapper>
     </CardContainer>
   );
